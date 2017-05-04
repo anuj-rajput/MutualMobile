91bbc53e6771a5b345330660b40b92c108c044b8
Anuj Rajput <anujrajput04@gmail.com>
2017-05-04 21:11:03 +0530

    Initial Commit

---
+++ MutualMobile/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MutualMobile
+//
+//  Created by Anuj Rajput on 04/05/17.
+//  Copyright © 2017 Anuj Rajput. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
